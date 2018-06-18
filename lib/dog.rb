762c8d46e5e4ac1dfcd48053b1a5c7e51df5be9b
Vishal Sharma <vsh1819@gmail.com>
2018-06-18 13:24:19 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+class Dog 
+  
+  @@all = []
+  
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name 
+    
+    @@all << self
+  end
+  
+  
+  
+  def self.all
+    
+    @@all.each do |dog|
+      puts "#{dog.name}"
+    end
+  end
+  
+  def self.clear_all
+    @@all.clear 
+  end
+end
